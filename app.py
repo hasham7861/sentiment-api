83d9fdcca71c8e1d27a8e2a73d971ebbdececc54
the-great-hasham <hasham@massively.ai>
2020-04-18 10:55:45 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify
+from sentifish import Sentiment
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return jsonify({
+        "status": "Api is running",
+        "suggestion": "/predict/<text> is the route to get sentiment analyze"
+    })
+
+
+@app.route('/predict/<text>')
+def predict_sentiment(text):
+    obj = Sentiment(text)
+    polarity = obj.analyze()
+    positive = obj.isPositive()
+    resp = jsonify(
+        {"input_text": text, "postive": positive, "polarity": polarity})
+
+    return resp
